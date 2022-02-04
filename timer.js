e3ad4bf42045def9a8bbe14689044c4d3bd9b6a6
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-02-04 12:45:46 +1100

    clear alarm function

--- timer.js
+++ timer.js
@@ -1,12 +1,22 @@
 const timer = () => {
     var start = document.getElementById("start");
     var alarmName = "pomTime";
+
     start.addEventListener('click',() => {
         console.log("pressed");
+        createAlarm();
+    });
+
+
+    function createAlarm() {
         chrome.alarms.create(alarmName, {
             delayInMinutes: 0.1
         });
-    });
+    }
+
+    function clearAlarm() {
+        chrome.alarms.clear(alarmName);
+    }
 }
 
 timer();
